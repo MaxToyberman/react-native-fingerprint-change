d307157540a8eb0aca91b780479cf3482f6d6c6d
Maxim Toyberman <>
2018-04-12 15:57:22 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNFingerprintChange } = NativeModules;
+
+export default RNFingerprintChange;
---
+++ ios/RNFingerprintChange.m
@@ -0,0 +1,50 @@
+
+#import "RNFingerprintChange.h"
+#import <LocalAuthentication/LocalAuthentication.h>
+
+@implementation RNFingerprintChange
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+
+RCT_EXPORT_METHOD(hasFingerPrintChanged:(RCTResponseSenderBlock)callback)
+{
+    BOOL changed = NO;
+
+    LAContext *context = [[LAContext alloc] init];
+    [context canEvaluatePolicy:LAPolicyDeviceOwnerAuthentication error:nil];
+    
+    NSData *domainState = [context evaluatedPolicyDomainState];
+    
+    // load the last domain state from touch id
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSData *oldDomainState = [defaults objectForKey:@"domainTouchID"];
+    
+    if (oldDomainState)
+    {
+        // check for domain state changes
+        
+        if ([oldDomainState isEqual:domainState])
+        {
+            NSLog(@"nothing changed.");
+        }
+        else
+        {
+            changed = YES;
+            NSLog(@"domain state was changed!");
+        }
+    }
+
+    // save the domain state that will be loaded next time
+    [defaults setObject:domainState forKey:@"domainTouchID"];
+    [defaults synchronize];
+    
+    callback(@[[NSNumber numberWithBool:changed]]);
+}
+
+@end
+  
